720306faca3a9ce65d5623de2356b8d251b3d7f8
igsr5 <igsr5.dev@gmail.com>
2023-01-07 17:24:45 +0900

    Fix

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ export const newApp = () => {
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     logLevel: LogLevel.DEBUG,
+    socketMode: false,
   });
 
   registerMiddlewares(app);
