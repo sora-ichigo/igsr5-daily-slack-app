6f52a668c7ed0a7b97729a43181b6781b8841ee1
igsr5 <igsr5.dev@gmail.com>
2023-01-07 17:44:41 +0900

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { newApp } from "./app";
 
 const prismaClient = new PrismaClient();
-const app = newApp();
 
+const app = newApp();
 (async () => {
   await app.start(Number(process.env.PORT) || 3000);
   console.log("⚡️ Bolt app is running!");
