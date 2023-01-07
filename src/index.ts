341cbc740bbaf081c9a85ef3ef8da604af97c1ed
igsr5 <igsr5.dev@gmail.com>
2023-01-07 17:17:37 +0900

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import { newApp } from "./app";
 const prismaClient = new PrismaClient();
 const app = newApp();
 
+console.log("port: ", process.env.PORT);
+console.log("tz: ", process.env.TZ);
+console.log("node_env: ", process.env.NODE_ENV);
+console.log("slack_bot_token: ", process.env.SLACK_BOT_TOKEN);
+console.log("slack_signing_secret: ", process.env.SLACK_SIGNING_SECRET);
+console.log("slack_admin_user_id: ", process.env.SLACK_ADMIN_USER_ID);
+console.log("database_url: ", process.env.DATABASE_URL);
+
 (async () => {
   await app.start(Number(process.env.PORT) || 3000);
   console.log("⚡️ Bolt app is running!");
