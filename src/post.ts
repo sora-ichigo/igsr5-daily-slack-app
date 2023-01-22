b3bd9ee87c1622fd10a62a4e13c8633a56fe2b17
igsr5 <igsr5.dev@gmail.com>
2023-01-22 18:36:20 +0900

    Support category

--- src/post.ts
+++ src/post.ts
@@ -1,4 +1,4 @@
-import { Post, PrismaClient } from "@prisma/client";
+import { Category, Post, PrismaClient } from "@prisma/client";
 
 type CreatePostInput = {
   title: string;
@@ -6,13 +6,43 @@ type CreatePostInput = {
   content: string;
 };
 export const createPost = async (client: PrismaClient, input: CreatePostInput): Promise<Post> => {
+  const publishedAt = new Date();
+
+  const categoryName = calculateCategoryName(publishedAt);
+  let category = await getCategoryByName(categoryName);
+  if (!category) {
+    category = await client.category.create({
+      data: {
+        name: categoryName,
+      },
+    });
+  }
+
   const post = await client.post.create({
     data: {
       title: input.title,
       subtitle: input.subtitle,
       content: input.content,
+      category_id: category.id,
+      published_at: publishedAt,
     },
   });
 
   return post;
+};
+
+const calculateCategoryName = (publishedAt: Date) => {
+  const year = publishedAt.getFullYear();
+  const month = publishedAt.getMonth() + 1;
+  return `reports of ${year}/${month}`;
+};
+
+const getCategoryByName = async (name: string): Promise<Category | null> => {
+  const prisma = new PrismaClient();
+  const category = await prisma.category.findUnique({
+    where: {
+      name,
+    },
+  });
+  return category;
 };
